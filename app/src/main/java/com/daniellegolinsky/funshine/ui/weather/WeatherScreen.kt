4859d8ef28d6d0c5e43090fcbd8f289a7f599809
Danielle Golinsky <degolinsky@icloud.com>
2023-07-04 16:45:13 -0400

    Added buttons for refreshing the page and yet-to-be-implemented settings pages.

--- app/src/main/java/com/daniellegolinsky/funshine/ui/weather/WeatherScreen.kt
+++ app/src/main/java/com/daniellegolinsky/funshine/ui/weather/WeatherScreen.kt
@@ -1,16 +1,23 @@
 package com.daniellegolinsky.funshine.ui.weather
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.daniellegolinsky.designsystem.*
+import com.daniellegolinsky.designsystem.components.FsButton
 import com.daniellegolinsky.designsystem.components.FsText
 import com.daniellegolinsky.designsystem.components.WeatherStatusImage
 import com.daniellegolinsky.designsystem.font.getBodyFontStyle
@@ -25,8 +32,11 @@ fun WeatherScreen(
     Column(
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.padding(32.dp) // TODO Obviously bad
+        modifier = modifier
+            .padding(32.dp) // TODO Obviously bad
+            .fillMaxSize()
     ) {
+        val context = LocalContext.current
         WeatherStatusImage(
             imageResource = R.drawable.ic_sunny_black,
             imageResourceContentDescription = R.string.ic_sunny_content_description,
@@ -42,6 +52,23 @@ fun WeatherScreen(
             textStyle = getBodyFontStyle(),
             maxLines = 8
         )
+        Spacer(modifier = Modifier.weight(1f))
+        Row (
+            horizontalArrangement = Arrangement.End,
+            modifier = Modifier.fillMaxWidth()
+                ){
+            FsButton(
+                buttonIcon = R.drawable.ic_settings_button_black,
+                buttonIconContentDescription = R.string.ic_settings_button_content_description,
+                onClick = { Toast.makeText(context, "What's wrong with my settings?", Toast.LENGTH_SHORT).show() }
+            )
+            Spacer(modifier = Modifier.width(2.dp))
+            FsButton(
+                buttonIcon = R.drawable.ic_refresh_button_black,
+                buttonIconContentDescription = R.string.ic_refresh_button_content_description,
+                onClick = { Toast.makeText(context, "How refreshing!", Toast.LENGTH_SHORT).show() }
+            )
+        }
     }
 }
 
---
+++ designsystem/src/main/java/com/daniellegolinsky/designsystem/components/FsButton.kt
@@ -0,0 +1,71 @@
+package com.daniellegolinsky.designsystem.components
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.daniellegolinsky.designsystem.R
+import com.daniellegolinsky.designsystem.designelements.getForegroundItemColor
+
+@Composable
+fun FsButton(
+    @DrawableRes buttonIcon: Int,
+    @StringRes buttonIconContentDescription: Int,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    Button(
+        shape = RectangleShape,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent,
+            disabledContainerColor = Color.Transparent,
+        ),
+        contentPadding = PaddingValues(0.dp),
+        modifier = modifier.height(FsButtonDefaults.BUTTON_HEIGHT),
+        onClick = { onClick() }
+    ) {
+        Icon(
+            painter = painterResource(id = buttonIcon),
+            contentDescription = stringResource(buttonIconContentDescription),
+            tint = getForegroundItemColor(),
+            modifier = Modifier.padding(0.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewFsButtonRefresh() {
+    FsButton(
+        buttonIcon = R.drawable.ic_refresh_button_black,
+        buttonIconContentDescription = R.string.ic_refresh_button_content_description,
+        onClick = {}
+    )
+}
+
+@Preview
+@Composable
+fun PreviewFsButtonSettings() {
+    FsButton(
+        buttonIcon = R.drawable.ic_settings_button_black,
+        buttonIconContentDescription = R.string.ic_settings_button_content_description,
+        onClick = {}
+    )
+}
+
+object FsButtonDefaults {
+    val BUTTON_HEIGHT = 32.dp
+}
