ba481af24e71d8e7a5b7064ab22e81868527c862
Danielle Golinsky <degolinsky@icloud.com>
2023-08-03 18:10:18 -0400

    Quick cleanup, it's just me, no pr needed ;)

--- app/src/main/java/com/daniellegolinsky/funshine/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/daniellegolinsky/funshine/ui/settings/SettingsScreen.kt
@@ -16,9 +16,6 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Switch
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
