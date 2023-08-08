23e0561f44df98a79127622e611b8c0336511bba
Danielle Golinsky <degolinsky@icloud.com>
2023-08-07 21:43:07 -0400

    With double large text (display size at maximum and text size at maximum), we needed scrolling. While this isn't necessary to support typically, I think it could help someone, and therefore is worth it.

--- app/src/main/java/com/daniellegolinsky/funshine/ui/weather/WeatherScreen.kt
+++ app/src/main/java/com/daniellegolinsky/funshine/ui/weather/WeatherScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
@@ -42,6 +44,7 @@ fun WeatherScreen(
         modifier = modifier
             .padding(32.dp) // TODO Obviously bad
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
     ) {
         FsIconWithShadow(
             image = painterResource(viewState.weatherIconResource),
