76f45d967e0213188017b0ccf3704623f95f27a2
Danielle Golinsky <degolinsky@icloud.com>
2024-10-22 15:39:07 -0400

    Moved to version catalog, began upgrade to latest versions of compose and kotlin.

---
+++ app/src/main/java/com/daniellegolinsky/funshine/navigation/NavigationViewModel.kt
@@ -0,0 +1,9 @@
+package com.daniellegolinsky.funshine.navigation
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class NavigationViewModel @Inject constructor() {
+
+}
