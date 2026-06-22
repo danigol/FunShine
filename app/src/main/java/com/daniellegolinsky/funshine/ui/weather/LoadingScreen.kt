91a23899103efd5ba43dd372b05fcc29aded8395
Danielle Golinsky <degolinsky@icloud.com>
2026-06-21 23:11:10 -0400

    Tweaked sizing

--- app/src/main/java/com/daniellegolinsky/funshine/ui/weather/LoadingScreen.kt
+++ app/src/main/java/com/daniellegolinsky/funshine/ui/weather/LoadingScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,7 +28,7 @@ fun LoadingScreen(
     Column(
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize().padding(16.dp)
     ) {
         Row(
             horizontalArrangement = Arrangement.Center,
